a059e3c8852e13cc92462c72ba97efa1657c0aac
s542932 <84051396+s542932@users.noreply.github.com>
2021-05-17 12:35:04 -0500

    Update bonus.py

--- bonus.py
+++ bonus.py
@@ -1,2 +1,5 @@
 Print('Dataset includes: 500, 33, 47, 99, 101, 17, 11, 2, 12, 6, 844, 323')\n\n
-print('The min
+print('The minimum value from the dataset is equal to:', min(500, 33, 47, 99, 101, 17, 11, 2, 12, 6, 844, 323))\n\n
+print('The maximum value from the dataset is equal to:', max(500, 33, 47, 99, 101, 17, 11, 2, 12, 6, 844, 323))\n\n
+print('The range of values for this dataset is equal to:', min(500, 33, 47, 99, 101, 17, 11, 2, 12, 6, 844, 323), 
+  '-', max(500, 33, 47, 99, 101, 17, 11, 2, 12, 6, 844, 323))
