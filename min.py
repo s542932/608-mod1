60fb4c18c57419aefce3872a38d03e4fbd01376d
s542932 <84051396+s542932@users.noreply.github.com>
2021-05-17 12:25:03 -0500

    Update min.py

--- min.py
+++ min.py
@@ -1,1 +1,1 @@
-print('minimum value from range of 47, 95, 88, 73, 88, & 84 is equal to:', min(47, 95, 88, 73, 88, 84))
+print('The minimum value from the dataset (47, 95, 88, 73, 88, & 84) is equal to:', min(47, 95, 88, 73, 88, 84))
