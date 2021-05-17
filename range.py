e10f20c88ba644f2e032cbc6aa6ecc92426e2bcb
s542932 <84051396+s542932@users.noreply.github.com>
2021-05-17 12:26:45 -0500

    Update range.py

--- range.py
+++ range.py
@@ -1,2 +1,2 @@
-print('Range:', min(47, 95, 88, 73, 88, 84), '-',
+print('The range of values from the dataset (47, 95, 88, 73, 88, & 84) is equal to:', min(47, 95, 88, 73, 88, 84), '-',
       max(47, 95, 88, 73, 88, 84))
